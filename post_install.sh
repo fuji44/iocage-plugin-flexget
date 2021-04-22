f9a620454f79b90a6371791d9e23f1568de5abab
fuji44 <fuji.44mt@gmail.com>
2021-04-22 20:19:15 +0900

    fix install process

--- post_install.sh
+++ post_install.sh
@@ -5,51 +5,52 @@ pw useradd flexget -s /bin/csh -m
 
 # Install flexget
 : ${flexget_venv_dir="/home/flexget/flexget"}
-python3.7 -m venv $flexget_venv_dir
-cd $flexget_venv_dir
-bin/pip install flexget
+sudo -u flexget python3.9 -m venv $flexget_venv_dir
+packages = "flexget"
 # Option: If Use Deluge plugin
 : ${use_deluge=false}
 if "${use_deluge}"; then
-    bin/pip install deluge-client
+    packages = "$packages deluge-client"
 fi
 # Option: If Use Transmission plugin
 : ${use_transmission=false}
 if "${use_transmission}"; then
-    bin/pip install transmission-rpc
+    packages = "$packages transmission-rpc"
 fi
 # Option: If Use Periscope plugin
 : ${use_periscope=false}
 if "${use_periscope}"; then
-    bin/pip install periscope
+    packages = "$packages periscope"
 fi
 # Option: If Use Subliminal plugin
 : ${use_subliminal=false}
 if "${use_subliminal}"; then
-    bin/pip install subliminal>=2.0
+    packages = "$packages subliminal>=2.0"
 fi
 # Option: If Use Telegram plugin
 : ${use_telegram=false}
 if "${use_telegram}"; then
-    bin/pip install python-telegram-bot==12.8
+    packages = "$packages python-telegram-bot==12.8"
 fi
 # Option: If Use XMPP plugin
 : ${use_xmpp=false}
 if "${use_xmpp}"; then
-    bin/pip install sleekxmpp
+    packages = "$packages sleekxmpp"
 fi
+echo "pip install packages: $packages"
+sudo -u flexget $flexget_venv_dir/bin/pip install $packages
 
 # Configure flexget
 echo "@reboot $flexget_venv_dir/bin/flexget daemon start -d" | crontab -u flexget -
+chown -R flexget:flexget /home/flexget/.config
+chown -R flexget:flexget /home/flexget/flexget
+
 : ${flexget_webui_password="Flex#get123"}
-$flexget_venv_dir/bin/flexget web passwd $flexget_webui_password
-chmod -R flexget:flexget /home/flexget/.config/flexget
-chmod -R flexget:flexget /home/flexget/flexget
+sudo -u flexget $flexget_venv_dir/bin/flexget web passwd $flexget_webui_password
 
 # Start flexget daemon
-su - flexget <<EOF
-/home/flexget/flexget/bin/flexget daemon start -d
-EOF
+sudo -u flexget $flexget_venv_dir/bin/flexget daemon start -d
+
 
 # Success messages
 echo "✅ flexget installation is complete!" > /root/PLUGIN_INFO
