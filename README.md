460436f2506a52d6ce95c361ee8b0ba5972b242d
fuji44 <fuji.44mt@gmail.com>
2021-04-22 22:51:17 +0900

    Delete unnecessary settings

--- README.md
+++ README.md
@@ -2,3 +2,64 @@
 Artifact file(s) for flexget iocage plugin
 
 https://flexget.com/
+
+## Installation
+
+```
+iocage fetch -P flexget -g https://github.com/fuji44/iocage-fuji44-plugins.git --branch main ip4_addr="em0|192.168.0.100/24"
+```
+
+## Usege
+
+After installation, please access the following URL.
+The default password for logging in to the web UI is `Flex#get123`.
+
+http://your_ip_addr:5050/
+
+
+## Configure
+
+Most settings can be changed in the Web UI.
+Some plugins require additional packages and must be installed manually.
+
+If you want to know more about flexget configuration, please see the following URL.
+
+https://flexget.com/Configuration
+
+
+### Advanced configure
+
+If you want to make settings that cannot be done with the Web UI, you have to access the jail console and execute commands or rewrite the configuration file.
+
+Since flexget is configured to run as the flexget user, use the `flexget` user to operate the console.
+
+configuration directory is `/home/flexget/.config/flexget`. Files such as logs, configurations and DBs can be found here.
+
+flexget is installed in `/home/flexget/flexget`.
+To execute the `flexget` command, activate the venv environment or specify the path.
+
+```
+su - flexget
+source /home/flexget/flexget/bin/activate
+flexget --help
+# or
+/home/flexget/flexget/bin/flexget --help
+```
+
+flexget starts in daemon mode. You can start / stop and check the status using the `flexget daemon` command.
+
+```
+flexget daemon start -d
+flexget daemon stop
+flexget daemon status
+```
+
+You need to run the command to change the password. It seems that it can be changed from the Web API as well.
+
+```
+flexget web passwd new_password
+```
+
+See the URL below for the Web API.
+
+http://your_ip_addr:5050/api
--- post_install.sh
+++ post_install.sh
@@ -6,39 +6,7 @@ pw useradd flexget -s /bin/csh -m
 # Install flexget
 : ${flexget_venv_dir="/home/flexget/flexget"}
 sudo -u flexget python3.9 -m venv $flexget_venv_dir
-packages="flexget"
-# Option: If Use Deluge plugin
-: ${use_deluge=false}
-if "${use_deluge}"; then
-    packages="$packages deluge-client"
-fi
-# Option: If Use Transmission plugin
-: ${use_transmission=false}
-if "${use_transmission}"; then
-    packages="$packages transmission-rpc"
-fi
-# Option: If Use Periscope plugin
-: ${use_periscope=false}
-if "${use_periscope}"; then
-    packages="$packages periscope"
-fi
-# Option: If Use Subliminal plugin
-: ${use_subliminal=false}
-if "${use_subliminal}"; then
-    packages="$packages subliminal>=2.0"
-fi
-# Option: If Use Telegram plugin
-: ${use_telegram=false}
-if "${use_telegram}"; then
-    packages="$packages python-telegram-bot==12.8"
-fi
-# Option: If Use XMPP plugin
-: ${use_xmpp=false}
-if "${use_xmpp}"; then
-    packages="$packages sleekxmpp"
-fi
-echo "pip install packages: $packages"
-sudo -u flexget $flexget_venv_dir/bin/pip install $packages
+sudo -u flexget $flexget_venv_dir/bin/pip install flexget
 
 # Configure flexget
 echo "@reboot $flexget_venv_dir/bin/flexget daemon start -d" | crontab -u flexget -
@@ -51,7 +19,6 @@ sudo -u flexget $flexget_venv_dir/bin/flexget web passwd $flexget_webui_password
 # Start flexget daemon
 sudo -u flexget $flexget_venv_dir/bin/flexget daemon start -d
 
-
 # Success messages
 echo "✅ flexget installation is complete!" > /root/PLUGIN_INFO
 echo "flexget is running in daemon mode." >> /root/PLUGIN_INFO
@@ -59,3 +26,6 @@ echo "" >> /root/PLUGIN_INFO
 echo "App dir: $flexget_venv_dir" >> /root/PLUGIN_INFO
 echo "Config dir: $flexget_config_dir" >> /root/PLUGIN_INFO
 echo "Webui password: $flexget_webui_password" >> /root/PLUGIN_INFO
+echo "" >> /root/PLUGIN_INFO
+echo "To change the password, execute the following command." >> /root/PLUGIN_INFO
+echo "$flexget_venv_dir/bin/flexget web passwd new_password" >> /root/PLUGIN_INFO
