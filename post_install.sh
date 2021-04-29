0e8fdb319418740c6b55ec84863a789898cd98f5
fuji44 <fuji.44mt@gmail.com>
2021-04-30 04:08:45 +0900

    Changed to launch via rc script

--- post_install.sh
+++ post_install.sh
@@ -1,32 +1,37 @@
 #!/bin/sh
 
+: ${flexget_app_dir="/home/flexget/flexget"}
+: ${flexget_config_dir="/home/flexget/.config"}
+: ${flexget_user="flexget"}
+
+sysrc -f /etc/rc.conf flexget_enable="YES"
+sysrc -f /etc/rc.conf flexget_app_dir=$flexget_app_dir
+sysrc -f /etc/rc.conf flexget_user=$flexget_user
+
 # Create run user
-pw useradd flexget -s /bin/csh -m
+pw useradd $flexget_user -s /bin/csh -m
 
 # Install flexget
-: ${flexget_venv_dir="/home/flexget/flexget"}
-sudo -u flexget python3.9 -m venv $flexget_venv_dir
-sudo -u flexget $flexget_venv_dir/bin/pip install flexget
+sudo -H -u $flexget_user /usr/local/bin/python3.9 -m venv $flexget_app_dir
+sudo -H -u $flexget_user $flexget_app_dir/bin/pip install flexget
 
 # Configure flexget
-: ${flexget_config_dir="/home/flexget/.config"}
-echo "@reboot $flexget_venv_dir/bin/flexget daemon start -d" | crontab -u flexget -
-chown -R flexget:flexget $flexget_config_dir
-chown -R flexget:flexget $flexget_venv_dir
+chown -R $flexget_user:$flexget_user $flexget_config_dir $flexget_app_dir 
+chmod +x /usr/local/bin/flexgetget.sh /usr/local/bin/flexgetget.sh
 
 : ${flexget_webui_password="Flex#get123"}
-sudo -u flexget $flexget_venv_dir/bin/flexget web passwd $flexget_webui_password
+sudo -H -u $flexget_user $flexget_app_dir/bin/flexget web passwd $flexget_webui_password
 
 # Start flexget daemon
-sudo -u flexget $flexget_venv_dir/bin/flexget daemon start -d
+sudo -H -u $flexget_user $flexget_app_dir/bin/flexget daemon start -d
 
 # Success messages
 echo "✅ flexget installation is complete!" > /root/PLUGIN_INFO
 echo "flexget is running in daemon mode." >> /root/PLUGIN_INFO
 echo "" >> /root/PLUGIN_INFO
-echo "App dir: $flexget_venv_dir" >> /root/PLUGIN_INFO
+echo "App dir: $flexget_app_dir" >> /root/PLUGIN_INFO
 echo "Config dir: $flexget_config_dir" >> /root/PLUGIN_INFO
 echo "Webui password: $flexget_webui_password" >> /root/PLUGIN_INFO
 echo "" >> /root/PLUGIN_INFO
 echo "To change the password, execute the following command." >> /root/PLUGIN_INFO
-echo "$flexget_venv_dir/bin/flexget web passwd new_password" >> /root/PLUGIN_INFO
+echo "$flexget_app_dir/bin/flexget web passwd new_password" >> /root/PLUGIN_INFO
