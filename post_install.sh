fc0fe23437990f34f8b5bb0d66ad9bf1803d4429
fuji44 <fuji.44mt@gmail.com>
2021-04-30 04:28:38 +0900

    Remove useless variables

--- post_install.sh
+++ post_install.sh
@@ -1,29 +1,28 @@
 #!/bin/sh
 
 : ${flexget_app_dir="/home/flexget/flexget"}
-: ${flexget_config_dir="/home/flexget/.config"}
-: ${flexget_user="flexget"}
+: ${flexget_config_dir="/home/flexget/.config/flexget"}
 
 sysrc -f /etc/rc.conf flexget_enable="YES"
 sysrc -f /etc/rc.conf flexget_app_dir=$flexget_app_dir
-sysrc -f /etc/rc.conf flexget_user=$flexget_user
+sysrc -f /etc/rc.conf flexget_user=flexget
 
 # Create run user
-pw useradd $flexget_user -s /bin/csh -m
+pw useradd flexget -s /bin/csh -m
 
 # Install flexget
-sudo -H -u $flexget_user /usr/local/bin/python3.9 -m venv $flexget_app_dir
-sudo -H -u $flexget_user $flexget_app_dir/bin/pip install flexget
+sudo -H -u flexget /usr/local/bin/python3.9 -m venv $flexget_app_dir
+sudo -H -u flexget $flexget_app_dir/bin/pip install flexget
 
 # Configure flexget
-chown -R $flexget_user:$flexget_user $flexget_config_dir $flexget_app_dir 
+chown -R flexget:flexget $flexget_config_dir $flexget_app_dir 
 chmod +x /usr/local/bin/flexgetget.sh /usr/local/bin/flexgetget.sh
 
 : ${flexget_webui_password="Flex#get123"}
-sudo -H -u $flexget_user $flexget_app_dir/bin/flexget web passwd $flexget_webui_password
+sudo -H -u flexget $flexget_app_dir/bin/flexget web passwd $flexget_webui_password
 
 # Start flexget daemon
-sudo -H -u $flexget_user $flexget_app_dir/bin/flexget daemon start -d
+sudo -H -u flexget $flexget_app_dir/bin/flexget daemon start -d
 
 # Success messages
 echo "✅ flexget installation is complete!" > /root/PLUGIN_INFO
